84258749a4ee4fe77e7d7dd9154f0ebb150559b2
christianangeles <christian.angeles@smoothstack.com>
2021-05-07 20:12:09 -0700

    SSOR-157 add navbar service

--- src/app/shared/layout/navbar/navbar.component.ts
+++ src/app/shared/layout/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavbarService } from '../../services/navbar.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor(public navlink: NavbarService, private redirect: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onLogout(){
+    this.navlink.isLoginViewable = true;
+    this.navlink.isProfileViewable = false;
+    this.redirect.navigate(['home']);
   }
-
 }
