9c2ff92ab8956a67f62de48f47f84995b7415c4e
Derrian Harris <80282599+DerrianH@users.noreply.github.com>
2021-05-23 16:18:19 -0500

    Feature/ssor 172 merge loan UI (#9)
    * SSOR-172 Initial merge
    
    * SSOR-172 Refactored to be inline with other components.
    
    * SSOR-172 Refactored to be inline with other components.
    
    * SSOR-172 Updated test cases
    
    Co-authored-by: Derrian Harris <dsh0018au@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,42 @@
 // Modules
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
-import {AppRoutingModule} from './app-routing.module';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {CommonModule, DatePipe} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule, DatePipe } from '@angular/common';
 
-import {TransactionHttpService} from './shared/services/transaction-http.service';
+import { TransactionHttpService } from './shared/services/transaction-http.service';
 
 // Components
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 
-import {TransactionViewComponent} from './transaction/components/transaction-view/transaction-view.component';
-import {TransactionTableComponent} from './transaction/components/transaction-view/transaction-table/transaction-table.component';
-import {TransactionSearchComponent} from './transaction/components/transaction-view/transaction-queries/transaction-search/transaction-search.component';
-import {TransactionFilterComponent} from './transaction/components/transaction-view/transaction-queries/transaction-filter/transaction-filter.component';
-import {TransactionQueriesComponent} from './transaction/components/transaction-view/transaction-queries/transaction-queries.component';
+import { TransactionViewComponent } from './transaction/components/transaction-view/transaction-view.component';
+import { TransactionTableComponent } from './transaction/components/transaction-view/transaction-table/transaction-table.component';
+import { TransactionSearchComponent } from './transaction/components/transaction-view/transaction-queries/transaction-search/transaction-search.component';
+import { TransactionFilterComponent } from './transaction/components/transaction-view/transaction-queries/transaction-filter/transaction-filter.component';
+import { TransactionQueriesComponent } from './transaction/components/transaction-view/transaction-queries/transaction-queries.component';
 
-import {NavbarComponent} from './shared/layout/navbar/navbar.component';
+import { NavbarComponent } from './shared/layout/navbar/navbar.component';
 
-import {ProfileComponent} from './user/profile/profile.component';
-import {ViewProfileComponent} from './user/profile/view-profile/view-profile.component';
-import {UpdateProfileComponent} from './user/profile/update-profile/update-profile.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { ViewProfileComponent } from './user/profile/view-profile/view-profile.component';
+import { UpdateProfileComponent } from './user/profile/update-profile/update-profile.component';
 
-import {HomeComponent} from './home/home.component';
+import { HomeComponent } from './home/home.component';
 
-import {RegisterComponent} from './login/register/register.component';
-import {LoginComponent} from './login/login.component';
-import {ForgotPasswordComponent} from './login/forgot-password/forgot-password.component';
-import {ResetPasswordComponent} from './login/reset-password/reset-password.component';
-import {AccountsListComponent} from './account/components/accounts-list/accounts-list.component';
-import {AccountTransactionViewComponent} from './account-transaction/components/account-transaction-view/account-transaction-view.component';
+import { RegisterComponent } from './login/register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './login/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './login/reset-password/reset-password.component';
+import { AccountsListComponent } from './account/components/accounts-list/accounts-list.component';
+import { AccountTransactionViewComponent } from './account-transaction/components/account-transaction-view/account-transaction-view.component';
 import { AccountsViewComponent } from './account/components/accounts-view/accounts-view.component';
-
+import { LoanTableComponent } from './loan/components/loan-table/loan-table.component';
+import { LoanViewComponent } from './loan/components/loan-view/loan-view.component';
 
 @NgModule({
   declarations: [
@@ -61,6 +62,9 @@ import { AccountsViewComponent } from './account/components/accounts-view/accoun
     AccountsListComponent,
     AccountTransactionViewComponent,
     AccountsViewComponent,
+
+    LoanViewComponent,
+    LoanTableComponent,
   ],
   imports: [
     BrowserModule,
@@ -76,5 +80,4 @@ import { AccountsViewComponent } from './account/components/accounts-view/accoun
   providers: [TransactionHttpService, DatePipe],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
